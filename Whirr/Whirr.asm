512899ccb28ea6d29c95b5aad407a44dd2c904bc
Rimmer van der Wal <rimmervdwal@gmail.com>
2018-11-02 09:59:25 +0100

    x64 assembly project labor of love

---
+++ Whirr/Whirr.asm
@@ -0,0 +1,23 @@
+include Macros.inc
+include Heaps.inc
+
+include Random.inc
+
+.DATA
+    WhirrHeapHandle                             DQ                  0
+
+; TEST
+    HeapData                                    DQ                  ?
+
+.CODE
+
+    WhirrProc PROC
+        call GetProcessHeap
+        mov WhirrHeapHandle, rax
+
+        _HeapAlloc HeapData, WhirrHeapHandle, 10000h
+
+        ret
+    WhirrProc ENDP
+
+END
