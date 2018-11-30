52ac9f24be1d0e2cf85cbe63938eff82896b17cb
Rimmer van der Wal <rimmervdwal@gmail.com>
2018-11-30 16:01:44 +0100

    Preparations for DXGI window

--- Whirr/Whirr.asm
+++ Whirr/Whirr.asm
@@ -3,6 +3,8 @@ include Macros.inc
 include Defines.inc
 include Externals.inc
 
+include COM.inc
+
 include Heaps.inc
 include Strings.inc
 
@@ -13,35 +15,63 @@ include Random.inc
     WHIRR_HEAPHANDLE                            DQ ?
 
     _String\
-        WHIRR_WINDOWCLASS,\
+        WHIRR_WINDOWCLASSNAME,\
         "WhirrWindowClass"
 
     _String\
         WHIRR_TITLE,\
         "Whirr"
 
+    ALIGN 8
+    WHIRR_WINDOWCLASS WNDCLASSEX\
+    {   sizeof WNDCLASSEX,\
+        CS_HREDRAW or CS_VREDRAW,\
+        WhirrWindowProc,\
+        0, 0, 0, 0, 0, 0, 0, WHIRR_WINDOWCLASSNAME, 0}
+
 .CODE
 
+    WhirrWindowProc PROC
+        call DefWindowProc
+        ret
+    WhirrWindowProc ENDP
+
     WhirrProc PROC
+    ; Initialize()
+    ; {
+        _Enter rcx, rdx, r8, r9
+
         xor rcx, rcx
         call GetModuleHandle
         mov WHIRR_MODULE, rax
 
         call GetProcessHeap
         mov WHIRR_HEAPHANDLE, rax
+    ; }
 
-        mov rcx, 10000000000000
+    ; ---
 
-        @Loop:
-            _Random rdx
-            or edx, edx
-            jz @Exit
-        loop @Loop
 
-        @Exit:
+        mov rcx, WHIRR_MODULE
+        lea rdx, WHIRR_WINDOWCLASSNAME
+
+        mov WHIRR_WINDOWCLASS.hInstance, rcx
+        mov WHIRR_WINDOWCLASS.lpszClassName, rdx
+
+        lea rcx, WHIRR_WINDOWCLASS
+        call RegisterClassEx
+        or rax, rax
+
+
+    ; ---
+
+    ; Finalize()
+    ; {
         xor rcx, rcx
         call ExitProcess
-        ret
+
+        _Leave
+    ; }
     WhirrProc ENDP
 
 END
